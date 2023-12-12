579df09a53de1c6221bd67cabd766047b2637861
Peter van den Hamer <github@vdhamer.com>
2023-12-12 22:24:36 +0100

    Initial Commit

---
+++ SwiftyJSON_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyJSON_demo
+//
+//  Created by Peter van den Hamer on 12/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
